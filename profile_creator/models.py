20781dbddaf931c24e5b671e311f1120fafbf591
Andrew Durrant <andrew.durrant75@gmail.com>
2020-08-31 17:08:41 -0600

    add initial views to views.py and User_Profile model to models.py; add create_view.html to templates

--- profile_creator/models.py
+++ profile_creator/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class User_Profile(models.Model):
+    first_name = models.CharField(max_length = 200)
+    last_name = models.CharField(max_length = 200)
+    nickname = models.CharField(max_length = 200)
+    email = models.EmailField(default = None)
+    display_picture = models.FileField()
+
+    def __str__(self):
+        return self.first_name
--- profile_creator/views.py
+++ profile_creator/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import (render, get_object_or_404, HttpResponseRedirect)
+from .forms import Profile_Form
+from .models import User_Profile
 
-# Create your views here.
+IMAGE_FILE_TYPES = ['png', 'jpg', 'jpeg']
+
+def create_profile(request):
+    form = Profile_Form()
+    if request.method == 'POST':
+        form = Profile_Form(request.POST, request.FILES)
+        if form.is_valid():
+            user_pr = form.save(commit=False)
+            user_pr.display_picture = request.FILES['display_picture']
+            file_type = user_pr.display_picture.url.split('.')[-1]
+            file_type = file_type.lower()
+            if file_type not in IMAGE_FILE_TYPES:
+                return render(request, 'profile_creator/error.html')
+            user_pr.save()
+            return render(request, 'profile_creator/details.html', {'user_pr': user_pr})
+    context = {"form": form,}
+    return render(request, 'profile_creator/create.html', context)
+
+
+def list_view(request):
+    context = {}
+    context['dataset'] = User_Profile.objects.all()
+
+    return render(request, 'profile_creator/list_view.html', context)
+
+def detail_view(request, id):
+    context = {}
+    context['data'] = User_Profile.objects.get(id = id)
+
+    return render(request, 'profile_creator/detail_view.html', context)
+
+def update_view(request, id):
+    context = {}
+
+    obj = get_object_or_404(User_Profile, id = id)
+
+    form = Profile_Form(request.POST or None, request.FILES or None, instance = obj)
+
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect('/'+id)
+
+    context['form'] = form
+
+    return render(request, 'profile_creator/update_view.html', context)
